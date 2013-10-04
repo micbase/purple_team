a0d12b5e60df8bece95d07e6047e5283828e7c12
Yang <yangzhang22@yahoo.com>
2013-10-04 14:17:37 -0500

    Yang

--- urls.py
+++ urls.py
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 from django.conf.urls import patterns, url
 from django.conf import settings
+=======
+from django.conf.urls import patterns, url #import two functions
+>>>>>>> Yang
 
 import dashboard.views
 
@@ -7,7 +11,11 @@ urlpatterns = patterns('',
     url(r'^$', dashboard.views.DashboardView.as_view(), name='dashboard-index'),
 )
 
+<<<<<<< HEAD
 if settings.DEBUG:
     urlpatterns += patterns('django.contrib.staticfiles.views',
         url(r'^static/(?P<path>.*)$', 'serve'),
     )
+=======
+#class based view
+>>>>>>> Yang

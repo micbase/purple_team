8ea9450eb38bbbe482a4e910c8d1678d52af4fb0
Qingyuan Zhang <miccrun@gmail.com>
2013-10-04 13:15:47 -0500

    add static files pattern for debug

--- urls.py
+++ urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
 
 import dashboard.views
 
 urlpatterns = patterns('',
     url(r'^$', dashboard.views.DashboardView.as_view(), name='dashboard-index'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    )

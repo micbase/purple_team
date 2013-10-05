d4cad3672158a75562cd23894533e0a1b3bd1f3f
sai <saikolli2012@u.northwestern.edu>
2013-10-05 16:21:54 -0500

    resolved conflicts

--- urls.py
+++ urls.py
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 from django.conf.urls import patterns, url
 from django.conf import settings
-=======
+
 from django.conf.urls import patterns, url #import two functions
->>>>>>> Yang
+
 
 import dashboard.views
 
@@ -11,11 +10,11 @@ urlpatterns = patterns('',
     url(r'^$', dashboard.views.DashboardView.as_view(), name='dashboard-index'),
 )
 
-<<<<<<< HEAD
+
 if settings.DEBUG:
     urlpatterns += patterns('django.contrib.staticfiles.views',
         url(r'^static/(?P<path>.*)$', 'serve'),
     )
-=======
+
 #class based view
->>>>>>> Yang
+

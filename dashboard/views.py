373859fe5d30911e381280c7de0e40e5c7a32ba1
Michael Chang <michael@micbase.com>
2013-10-05 17:26:33 -0500

    complete the backend logic

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,13 @@
 
 from django.db.models import Count
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from dashboard.models import UserSkill
 
 
-class DashboardView(TemplateView):
+class DashboardView(ListView):
     template_name = 'dashboard/index.html'
+    paginate_by = 10
 
     def get_queryset(self):
         s_id = 3
@@ -31,13 +32,12 @@ class DashboardView(TemplateView):
         )
 
         uid_list = [item.user_id for item in weakness_match]
-        final_match = [item for item in strength_match if item.user_id in uid_list]
-        import pdb;pdb.set_trace()
+        final_user = [item.user for item in strength_match
+                if item.user_id in uid_list]
 
-        return final_match
+        return final_user
 
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
         context['text'] = 'Hello World, Purple Team'
-        self.get_queryset()
         return context

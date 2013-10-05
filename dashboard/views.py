0c6c2e88482b11d68f5062bb1a32365480420b79
Qingyuan Zhang <miccrun@gmail.com>
2013-10-05 16:51:27 -0500

    fix the column name in schema

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,43 @@
 
+from django.db.models import Count
 from django.views.generic import TemplateView
+
+from dashboard.models import UserSkill
 
 
 class DashboardView(TemplateView):
     template_name = 'dashboard/index.html'
 
+    def get_queryset(self):
+        s_id = 3
+        w_id = 4
+
+        strength_match = UserSkill.objects.filter(
+            skill_id=s_id,
+            scale__lte=5,
+        ).annotate(
+            strength_counter=Count('id'),
+        ).filter(
+            strength_counter__gte=1,
+        )
+
+        weakness_match = UserSkill.objects.filter(
+            skill_id=w_id,
+            scale__gte=6,
+        ).annotate(
+            weakness_counter=Count('id'),
+        ).filter(
+            weakness_counter__gte=1,
+        )
+
+        uid_list = [item.user_id for item in weakness_match]
+        final_match = [item for item in strength_match if item.user_id in uid_list]
+        import pdb;pdb.set_trace()
+
+        return final_match
+
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
         context['text'] = 'Hello World, Purple Team'
+        self.get_queryset()
         return context
--- scripts/initial_data.py
+++ scripts/initial_data.py
@@ -2,57 +2,59 @@ from dashboard.models import User, Skill, UserSkill
 
 user_1 = User(email="zhuxiaofengWWW@gmail.com", password="purpleTeam",
 	 first_name="John", last_name="Shay",
-	phonenumber="7739881897", bio="I'm a freshman student of EECS department in Northwestern University")
+	phonenumber="7739881897", bio="I'm a freshman student of EECS department")
 user_1.save()
 skill_1 = Skill(skill_name="Dancing")
 skill_1.save()
 
 user_2 = User(email="zhuxiaofengwww@126.com", password="purpleTeam",
 	 first_name="Tylor", last_name="Smith",
-	phonenumber="8478078707", bio="Professional Dancer/Social Media Marketing Strategist,Featured Dancer and Triple Threat")
+	phonenumber="8478078707", bio="Dance Instructor")
 user_2.save()
 skill_2 = Skill(skill_name="Java")
 skill_2.save()
 
 user_3 = User(email="zhuxiaofengWWW@sina.com",password="purpleTeam",
 	 first_name="Julia", last_name="Linken",
-	phonenumber="7739881897", bio="Five years of experience as a Chef in Loews Boston Hotel")
+	phonenumber="7739881897", bio="Chef")
 user_3.save()
 skill_3 = Skill(skill_name="Cooking")
 skill_3.save()
 
-user_4 = User(email="982388314@qq.com",password="purpleTeam",
+user_4 = User(email="zhuxiaofengwww@sohu.com",password="purpleTeam",
 	 first_name="Andrew", last_name="Bush",
-	phonenumber="7739881897", bio="Vice-President of WaveCrest Software, Inc.")
+	phonenumber="7739881897", bio="Senior Software Engineer")
 user_4.save()
+
+user_5 = User(email="ddzhuxiaofengwww@sohu.com",password="purpleTeam",
+	 first_name="BBAndrew", last_name="ABush",
+	phonenumber="7739881897", bio="Engineer")
+user_5.save()
 
 skill_4 = Skill(skill_name="Table Tennis")
 skill_4.save()
 
-uskill_1=UserSkill(uid=user_1,skill_id=skill_1,scale =1)
+uskill_1=UserSkill(user=user_1,skill=skill_4,scale =1)
 uskill_1.save()
-uskill_2=UserSkill(uid=user_2,skill_id=skill_1,scale=10)
+uskill_2=UserSkill(user=user_2,skill=skill_2,scale=2)
 uskill_2.save()
-uskill_3=UserSkill(uid=user_3,skill_id=skill_3,scale=10)
+uskill_3=UserSkill(user=user_3,skill=skill_3,scale=3)
 uskill_3.save()
-uskill_4=UserSkill(uid=user_4,skill_id=skill_4,scale=4)
+uskill_4=UserSkill(user=user_4,skill=skill_4,scale=4)
 uskill_4.save()
-uskill_5=UserSkill(uid=user_1,skill_id=skill_2,scale=2)
+uskill_5=UserSkill(user=user_1,skill=skill_2,scale=5)
 uskill_5.save()
-uskill_6=UserSkill(uid=user_2,skill_id=skill_3,scale=6)
+uskill_6=UserSkill(user=user_2,skill=skill_3,scale=6)
 uskill_6.save()
-uskill_7=UserSkill(uid=user_3,skill_id=skill_4,scale=7)
+uskill_7=UserSkill(user=user_3,skill=skill_4,scale=7)
 uskill_7.save()
-uskill_8=UserSkill(uid=user_4,skill_id=skill_1,scale=1)
+uskill_8=UserSkill(user=user_4,skill=skill_1,scale=8)
 uskill_8.save()
-uskill_9=UserSkill(uid=user_1,skill_id=skill_3,scale=6)
+uskill_9=UserSkill(user=user_1,skill=skill_3,scale=9)
 uskill_9.save()
-uskill_10=UserSkill(uid=user_2,skill_id=skill_4,scale=7)
+uskill_10=UserSkill(user=user_2,skill=skill_4,scale=10)
 uskill_10.save()
-uskill_11=UserSkill(uid=user_3,skill_id=skill_1,scale=2)
+uskill_11=UserSkill(user=user_5,skill=skill_4,scale=10)
 uskill_11.save()
-uskill_12=UserSkill(uid=user_4,skill_id=skill_2,scale=10)
+uskill_12=UserSkill(user=user_5,skill=skill_3,scale=1)
 uskill_12.save()
-
-
-

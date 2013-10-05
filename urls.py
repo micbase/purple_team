0aed50740db40831fe08db17039debf0e931ac39
Michael Chang <michael@micbase.com>
2013-10-05 18:20:09 -0500

    add a 'result' endpoint

--- urls.py
+++ urls.py
@@ -1,20 +1,14 @@
 from django.conf.urls import patterns, url
 from django.conf import settings
 
-from django.conf.urls import patterns, url #import two functions
-
-
 import dashboard.views
 
 urlpatterns = patterns('',
     url(r'^$', dashboard.views.DashboardView.as_view(), name='dashboard-index'),
+    url(r'^result$', dashboard.views.ResultView.as_view(), name='result'),
 )
-
 
 if settings.DEBUG:
     urlpatterns += patterns('django.contrib.staticfiles.views',
         url(r'^static/(?P<path>.*)$', 'serve'),
     )
-
-#class based view
-

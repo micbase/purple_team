5dd9cfe1b85cefb070ebcae43cc0ec8fad9e076f
sai <saikolli2012@u.northwestern.edu>
2013-10-21 08:38:58 -0500

    create pages displaying but not working

--- urls.py
+++ urls.py
@@ -7,6 +7,10 @@ urlpatterns = patterns('',
     url(r'^$', dashboard.views.DashboardView.as_view(), name='dashboard-index'),
     url(r'^result$', dashboard.views.TopicsView.as_view(), name='result'),
     url(r'^posts/(?P<group_id>\d+)$', dashboard.views.PostsView.as_view(), name='group-info'),
+    url(r'^posts/post_content$', dashboard.views.CreatePostView.as_view(), name='group-info'),
+    url(r'^posts/result$', dashboard.views.CreatePostContentView.as_view(), name='group-info'),
+    url(r'^create_topic$', dashboard.views.CreateTopicView.as_view(), name='group-info'),
+
 )
 
 if settings.DEBUG:

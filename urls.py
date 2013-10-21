270479dba3c977142efb920b4ad823120e5009c1
Qingyuan Zhang <miccrun@gmail.com>
2013-10-21 09:34:05 -0500

    fix integration bugs

--- urls.py
+++ urls.py
@@ -4,9 +4,10 @@ from django.conf import settings
 import dashboard.views
 
 urlpatterns = patterns('',
-    url(r'^$', dashboard.views.DashboardView.as_view(), name='dashboard-index'),
-    url(r'^result$', dashboard.views.TopicsView.as_view(), name='result'),
-    url(r'^posts/(?P<group_id>\d+)$', dashboard.views.PostsView.as_view(), name='group-info'),
+    url(r'^$', dashboard.views.ResultView.as_view(), name='dashboard-index'),
+    url(r'^result$', dashboard.views.ResultView.as_view(), name='result'),
+    url(r'^topics/(?P<class_id>\d+)$', dashboard.views.TopicsView.as_view(), name='topics'),
+    url(r'^posts/(?P<topic_id>\d+)$', dashboard.views.PostsView.as_view(), name='group-info'),
     url(r'^posts/post_content$', dashboard.views.CreatePostView.as_view(), name='group-info'),
     url(r'^posts/result$', dashboard.views.CreatePostContentView.as_view(), name='group-info'),
     url(r'^create_topic$', dashboard.views.CreateTopicView.as_view(), name='group-info'),

03c2341ea57c3c56e0c1edda09b5c7c85df32fa4
Qingyuan Zhang <miccrun@gmail.com>
2013-10-25 08:26:13 -0500

    creating post requires login first

--- auth/views.py
+++ auth/views.py
@@ -1,9 +1,11 @@
 
 from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.utils.decorators import method_decorator
 from django.views.generic import (
-    View,
     FormView,
+    View,
 )
 
 from auth.forms import (
@@ -12,8 +14,14 @@ from auth.forms import (
 )
 
 
+class LoginRequiredMixin(object):
+
+    @method_decorator(login_required(redirect_field_name='redirect'))
+    def dispatch(self, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
+
+
 class AuthView(FormView):
-    success_url = '/'
 
     def form_valid(self, form):
         user = form.save()
@@ -25,10 +33,19 @@ class LoginView(AuthView):
     template_name = 'dashboard/login.html'
     form_class = LoginForm
 
+    def get_success_url(self):
+        return self.request.POST.get('redirect', '/')
+
+    def get_context_data(self, **kwargs):
+        context = super(LoginView, self).get_context_data(**kwargs)
+        context['redirect'] = self.request.GET.get('redirect', '/')
+        return context
+
 
 class RegisterView(AuthView):
     template_name = 'dashboard/register.html'
     form_class = RegisterForm
+    success_url = '/'
 
 
 class LogoutView(View):
